62cf695fd7ef9cf43abef668d6b937b57893b9b2
Chris <chris79gp@outlook.com>
2025-02-15 22:36:35 +0000

    Feat: add implementation of `.append()` and `.prepend()` methods

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,5 +1,39 @@
+import Node from "./Node";
+
 class LinkedList {
-	return;
+	constructor(head = null) {
+		this.head = head;
+		this.length = 0;
+	}
+
+	append(value) {
+		const node = new Node(value);
+
+		if (!this.head) {
+			this.head = node;
+		} else {
+			let current = this.head;
+
+			while (current.next) {
+				current = current.next;
+			}
+
+			current.setNext(node);
+		}
+
+		this.length += 1;
+	}
+
+	prepend(value) {
+		const node = new Node(value);
+
+		if (this.head) {
+			node.next = this.head;
+		}
+
+		this.head = node;
+		this.length += 1;
+	}
 }
 
 export default LinkedList;
